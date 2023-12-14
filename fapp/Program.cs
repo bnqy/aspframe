e09a62518f8c99b1711b55ac4860ab62612b287f
Beneqi <obbonobo@gmail.com>
2023-12-14 11:42:27 +0600

    add middleware endpoint

--- fapp/Program.cs
+++ fapp/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+	app.UseHttpLogging();
+}
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Dua", "Lipa"));
 
 app.Run();
+
+
+public record Person (string FistName, string LastName);
