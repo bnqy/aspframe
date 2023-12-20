90f0f23674e4e4753fe0ac606042aff7c84256e0
Beneqi <obbonobo@gmail.com>
2023-12-20 14:32:40 +0600

    UseExceptionHandler

--- middlewarepipline/Program.cs
+++ middlewarepipline/Program.cs
@@ -4,11 +4,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
 
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/error");
+}
 app.UseWelcomePage("/");
 app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.MapGet("/error", () => "Sorry, an error occurred");
 app.MapGet("/", () => "Hello world!");
 
 
