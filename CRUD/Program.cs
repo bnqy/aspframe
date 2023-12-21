86641312bf0ee934d4fec215a3c7b7f2a46deb39
Beneqi <obbonobo@gmail.com>
2023-12-21 14:14:03 +0600

    route handlers with methods

---
+++ CRUD/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/fruit", () => Fruit.All);
+
+var getFruit = (string id) => Fruit.All[id];
+app.MapGet("/fruit/{id}", getFruit);
+
+app.MapPost("/fruit/{id}", Handlers.AddFruit);
+
+Handlers handlers = new();
+app.MapPut("/fruit/{id}", handlers.ReplaceFruit);
+
+app.MapDelete("/fruit/{id}", DeleteFruit);
+
+app.Run();
+
+void DeleteFruit(string id)
+{
+	Fruit.All.Remove(id);
+}
+
+record Fruit(string Name, int Stock)
+{
+	public static readonly Dictionary<string, Fruit> All = new();
+};
+
+class Handlers
+{
+	public void ReplaceFruit(string id, Fruit fruit)
+	{
+		Fruit.All[id] = fruit;
+	}
+
+	public static void AddFruit(string id, Fruit fruit)
+	{
+		Fruit.All.Add(id, fruit);
+	}
+}
