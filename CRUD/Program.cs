9378826376ba6bbfbc3db98ec2e4953c611a9f9d
Beneqi <obbonobo@gmail.com>
2023-12-21 16:10:23 +0600

    Postman testing

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+app.MapGet("/", () => "Hello!");
 app.MapGet("/fruit", () => Fruit.All);
 
 var getFruit = (string id) => Fruit.All[id];
