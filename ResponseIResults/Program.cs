6db7bcfca519e2727eed65f0061cd1ef90b03f02
Beneqi <obbonobo@gmail.com>
2024-02-09 18:13:48 +0600

    endpoint filter

--- ResponseIResults/Program.cs
+++ ResponseIResults/Program.cs
@@ -3,30 +3,51 @@ using System.Collections.Concurrent;
 using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddProblemDetails();
 
+
 var app = builder.Build();
+
 
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler();
 }
-
 app.UseStatusCodePages();
 
 var fruits = new ConcurrentDictionary<string, Fruit>();
 
+
 app.MapGet("/fruits", () => fruits);
 
-app.MapGet("/fruit/{id}", (string id) => fruits.TryGetValue(id, out var fruit) 
+/*app.MapGet("/fruit/{id}", (string id) =>
+{
+	if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+	{
+		return Results.ValidationProblem(new Dictionary<string, string[]>
+		{
+			{"id", new[] {"Invalid format. Id must start with 'f'"}}
+		});
+	}
+
+	return fruits.TryGetValue(id, out var fruit)
+	? TypedResults.Ok(fruit)   // returns 2000 ok res with json
+							   // : Results.NotFound()	   
+	: Results.Problem(statusCode: 404);   // if id does not exist returns 404 not found res
+});*/
+
+app.MapGet("/fruit/{id}", (string id) =>
+fruits.TryGetValue(id, out var fruit)
 ? TypedResults.Ok(fruit)   // returns 2000 ok res with json
-// : Results.NotFound()
-: Results.Problem(statusCode: 404));   // if id does not exist returns 404 not found res
+							   // : Results.NotFound()	   
+: Results.Problem(statusCode: 404)) // if id does not exist returns 404 not found res
+	.AddEndpointFilter(ValidHepler.ValidateId);   // adds filter
+
+
 
 app.MapPost("/fruit/{id}", (string id, Fruit fruit) => fruits.TryAdd(id, fruit)
 ? TypedResults.Created($"/fruit/{id}", fruit)                    // returns 201 created response wirh json
-//: Results.BadRequest(new { id = "this id is already exists"})
+ //: Results.BadRequest(new { id = "this id is already exists"})
 : Results.ValidationProblem(new Dictionary<string, string[]>()
 {
 	{"id", new[] {"A fruit with this id already exists"}}
@@ -45,6 +66,7 @@ app.MapDelete("/fruit/{id}", (string id) =>
 });
 
 
+
 app.MapGet("/httpresponse", (HttpResponse resp) =>
 {
 	resp.StatusCode = 418;
@@ -52,9 +74,38 @@ app.MapGet("/httpresponse", (HttpResponse resp) =>
 	return resp.WriteAsync("It is a resp with Httpresponse!");
 });
 
+
 app.MapGet("/", void () => throw new Exception());
 app.MapGet("/sc", () => Results.NotFound());
 
 app.Run();
 
+
+
+
+
+
+
+
 record Fruit(string Name, int stock);
+
+class ValidHepler
+{
+	public static async ValueTask<object?> ValidateId(
+		EndpointFilterInvocationContext context,   // exposes endpoint arguments and HttpContext
+		EndpointFilterDelegate next)  // next filter or endpoint
+	{
+		var id = context.GetArgument<string>(0);  //gets first argument in the req
+
+		if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+		{
+			return Results.ValidationProblem(new Dictionary<string, string[]>
+			{
+				{"id", new[] {"Invalid format. Id must start with 'f'"}}
+			});
+		}
+
+		return await next(context);   // calls next filters
+	}
+
+}
