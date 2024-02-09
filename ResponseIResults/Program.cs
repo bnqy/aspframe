e967c7a37b415a489cd8e18ddaa3207803b07f7c
Beneqi <obbonobo@gmail.com>
2024-02-09 22:05:21 +0600

    iendepointfilter interface

--- ResponseIResults/Program.cs
+++ ResponseIResults/Program.cs
@@ -74,7 +74,8 @@ app.MapDelete("/fruit/{id}", (string id) =>
 {
 	fruits.TryRemove(id, out _);
 	return Results.NoContent();
-});
+})
+	.AddEndpointFilter<IdValidFilter>();
 
 
 
@@ -163,4 +164,22 @@ class ValidHepler
 		};
 	}
 
+}
+
+class IdValidFilter : IEndpointFilter
+{
+	public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+	{
+		var id = context.GetArgument<string>(0);
+
+		if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
+		{
+			return Results.ValidationProblem(new Dictionary<string, string[]>()
+			{
+				{"id", new[] {"Invalid format. Id must start with 'f'"}}
+			});
+		}
+
+		return await next(context);
+	}
 }
