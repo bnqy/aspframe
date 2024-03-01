19bd6ea3af6ea48bf052fa217decd93f858d316f
Beneqi <obbonobo@gmail.com>
2024-03-01 16:12:08 +0300

    generating client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Fruit;
+
+var client = new FruitClient("https://localhost:7286/",   // base url of API
+	new HttpClient());   // is used to call the API
+
+Fruit.Fruit created = await client.Fruit2Async("123", new Fruit.Fruit { Name="apple", Stock=32});  //Post
+Fruit.Fruit fetched = await client.FruitAsync("123");
+
+Console.WriteLine($"Fetched fruit: {fetched.Name}: {fetched.Stock}");
+
+Console.WriteLine("Hello, World!");
