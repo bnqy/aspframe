00ade83235ed20cffa5c175f1ebcf2012bdbb325
Beneqi <obbonobo@gmail.com>
2024-03-02 23:39:33 +0300

    post get

--- EFCore_Recipe/Program.cs
+++ EFCore_Recipe/Program.cs
@@ -1,9 +1,15 @@
 // 12
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => x.SwaggerDoc("v1", new OpenApiInfo { Title="Recipe App", Version="v1"}));
 
 // connstr taken from config, from ConnectionStrings section
 var connectionStringSqlite = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -15,6 +21,8 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionStrin
 //builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionStringSqlServer!));
 
 var app = builder.Build();
+
+app.UseSwagger(); app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
@@ -28,7 +36,7 @@ public class AppDbContext
 	{
 	}
 
-	public DbSet<Recipe> Recipes { get; set; }   // will be used to query db
+	public DbSet<Recipe> Recipes { get; set; }   // will be used to query db IQueryable
 }
 
 public class Recipe
@@ -51,4 +59,105 @@ public class Ingredient
 	public decimal Quantity { get; set; }
 	public int JustMigrate { get; set; }
 	public required string Unit { get; set; }
+}
+
+
+public class EditRecipeBase
+{
+	[Required, StringLength(100)]
+	public required string Name { get; set; }
+	[Range(0, 23), DisplayName("Time to cook (hrs)")]
+	public int TimeToCookHrs { get; set; }
+	[Range(0, 59), DisplayName("Time to cook (mins)")]
+	public int TimeToCookMins { get; set; }
+	[Required]
+	public required string Method { get; set; }
+	[DisplayName("Vegetarian?")]
+	public bool IsVegetarian { get; set; }
+	[DisplayName("Vegan?")]
+	public bool IsVegan { get; set; }
+}
+
+public class CreateRecipeCommand : EditRecipeBase
+{
+	public IList<CreateIngredientCommand> Ingredients { get; set; } = new List<CreateIngredientCommand>();
+
+	public Recipe ToRecipe()
+	{
+		return new Recipe
+		{
+			Name = Name,
+			TimeToCook = new TimeSpan(TimeToCookHrs, TimeToCookMins, 0),
+			Method = Method,
+			IsVegetarian = IsVegetarian,
+			IsVegan = IsVegan,
+			Ingredients = Ingredients.Select(x => x.ToIngredient()).ToList()
+		};
+	}
+}
+public class CreateIngredientCommand
+{
+	[Required, StringLength(100)]
+	public required string Name { get; set; }
+	[Range(0, int.MaxValue)]
+	public decimal Quantity { get; set; }
+	[Required, StringLength(20)]
+	public required string Unit { get; set; }
+
+	public Ingredient ToIngredient()
+	{
+		return new Ingredient
+		{
+			Name = Name,
+			Quantity = Quantity,
+			Unit = Unit,
+		};
+	}
+}
+
+public class RecipeDetailViewModel
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public required string Method { get; set; }
+
+	public required IEnumerable<Item> Ingredients { get; set; }
+
+	public class Item
+	{
+		public required string Name { get; set; }
+		public required string Quantity { get; set; }
+	}
+}
+
+public class RecipeSummaryViewModel
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public required string TimeToCook { get; set; }
+	public int NumberOfIngredients { get; set; }
+
+	public static RecipeSummaryViewModel FromRecipe(Recipe recipe)
+	{
+		return new RecipeSummaryViewModel
+		{
+			Id = recipe.RecipeId,
+			Name = recipe.Name,
+			TimeToCook = $"{recipe.TimeToCook.Hours}hrs {recipe.TimeToCook.Minutes}mins",
+		};
+	}
+}
+
+public class UpdateRecipeCommand : EditRecipeBase
+{
+	public int Id { get; set; }
+
+	public void UpdateRecipe(Recipe recipe)
+	{
+		recipe.Name = Name;
+		recipe.TimeToCook = new TimeSpan(TimeToCookHrs, TimeToCookMins, 0);
+		recipe.Method = Method;
+		recipe.IsVegetarian = IsVegetarian;
+		recipe.IsVegan = IsVegan;
+	}
 }
---
+++ EFCore_Recipe/RecipeService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Recipe;
+
+public class RecipeService
+{
+	readonly AppDbContext appDbContext;
+
+	public async Task<int> CreateRecipe(CreateRecipeCommand cmd)
+	{
+		var recipe = new Recipe
+		{
+			Name = cmd.Name,
+			TimeToCook = new TimeSpan(cmd.TimeToCookHrs, cmd.TimeToCookMins, 0),
+			Method = cmd.Method,
+			IsVegan = cmd.IsVegan,
+			IsVegetarian = cmd.IsVegetarian,
+			Ingredients = cmd.Ingredients.Select(i =>
+			new Ingredient
+			{
+				Name = i.Name,
+				Quantity = i.Quantity,
+				Unit = i.Unit,
+			})
+			.ToList(),
+		};
+
+		appDbContext.Add(recipe);
+		appDbContext.SaveChangesAsync();
+
+		return recipe.RecipeId;
+	}
+
+	public async Task<ICollection<RecipeSummaryViewModel>> GetRecipes()
+	{
+		return await appDbContext.Recipes
+			.Where(x => !x.IsDeleted)
+			.Select(y => new RecipeSummaryViewModel
+			{
+				Id = y.RecipeId,
+				Name = y.Name,
+				TimeToCook = $"{y.TimeToCook.TotalMinutes}mins"
+			})
+			.ToListAsync();
+	}
+
+	public async Task<RecipeDetailViewModel?> GetRecipeDetail(int id)
+	{
+		return await appDbContext.Recipes
+			.Where(x => x.RecipeId == id)
+			.Where(x => !x.IsDeleted)
+			.Select(x => new RecipeDetailViewModel
+			{
+				Id = x.RecipeId,
+				Name = x.Name,
+				Method = x.Method,
+				Ingredients = x.Ingredients
+				.Select(item => new RecipeDetailViewModel.Item
+				{
+					Name = item.Name,
+					Quantity = $"{item.Quantity} {item.Unit}"
+				})
+			})
+			.SingleOrDefaultAsync();
+	}
+}
