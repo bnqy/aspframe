8e9323f9d993f33039a8ba3a7c015c0cf3f8a98c
Beneqi <obbonobo@gmail.com>
2024-03-14 23:53:23 +0300

    start and imports

---
+++ RazorPagesView/Pages/ToDo/ViewToDo.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesView.Pages.ToDo
+{
+    public class ViewToDoModel : PageModel
+    {
+        private readonly TaskService _taskService;
+
+        public ViewToDoModel(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        public ToDoItemViewModel Item { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Item = _taskService.AllTasks.FirstOrDefault(x => x.Id == id);
+
+            if (Item == null)
+            {
+                return RedirectToPage("RecentToDos");
+            }
+            return Page();
+        }
+    }
+}
--- RazorPagesView/Program.cs
+++ RazorPagesView/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<TaskService>();
 
 var app = builder.Build();
 
@@ -23,3 +24,23 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+public record ToDoItemViewModel(int Id, string Title, params string[] Tasks)
+{
+    public bool IsComplete => Tasks.Length == 0;
+}
+
+public class TaskService
+{
+    //This list would be loaded from a database or file etc
+    public List<ToDoItemViewModel> AllTasks { get; } = new List<ToDoItemViewModel>
+        {
+            new ToDoItemViewModel(1, "Shopping List", "Buy milk", "Buy eggs", "Buy bread"),
+            new ToDoItemViewModel(2, "Agenda", "Pick up kids", "Take kids to school"),
+            new ToDoItemViewModel(4, "Car stuff", "Get fuel", "Check oil", "Check Tyre pressure"),
+            new ToDoItemViewModel(4, "Problems"),
+            new ToDoItemViewModel(5, "Writing tasks","Write blog post", "Edit book chapter"),
+        };
+
+}
