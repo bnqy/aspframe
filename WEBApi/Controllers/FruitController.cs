462d708f48a02dfcdf2ee2f6708be6ff7d7d7bf2
Beneqi <obbonobo@gmail.com>
2024-03-19 13:21:11 +0300

    routing with [RouteAttributes]

--- WEBApi/Controllers/FruitController.cs
+++ WEBApi/Controllers/FruitController.cs
@@ -12,12 +12,16 @@ namespace WEBApi.Controllers
 		}
 
 		[HttpGet("fruit")]  // defines the route template so -> /fruit
+		//[Route("")]  // can have multiple [RouteAttributes]
+		//[Route("/hi")]
 		public IEnumerable<string> Index()  // the name of action is not used for routing
 		{
 			return _fruitService.Fruit;
 		}
 
 		[HttpGet("fruit/{id}")]  // so -> /fruit/{id}
+		//[Route("view/{id}")]
+		//[Route("view-hi/{id}")]
 		public ActionResult<string> View(int id)  // it can return string or an IActionResult
 		{
 			if (id >= 0 && id < _fruitService.Fruit.Count)
