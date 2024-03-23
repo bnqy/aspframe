c5edc70f2420f2527efc5d20ec1b1b28161164b1
bnqy <obbonobo@gmail.com>
2024-03-23 14:41:48 +0300

    filters for actions and rp

---
+++ WEBApi/Filters/A_LogAsyncResourceFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WEBApi.Filters
+{
+    public class A_LogAsyncResourceFilter : Attribute, IAsyncResourceFilter
+    {
+        // Execured at the start of the pipline after author
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, 
+            ResourceExecutionDelegate next) // encapsulates the remainder of the pipline
+        {
+            Console.WriteLine("Executing async!!!");
+
+            // executes rest of the pipe and gets ResourceExecutedContext
+            ResourceExecutedContext resourceExecutedContext = await next();
+            Console.WriteLine("Executed async!"); // called at the end
+        }
+    }
+}
---
+++ WEBApi/Filters/LogResourceFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WEBApi.Filters;
+
+public class LogResourceFilter : Attribute, IResourceFilter
+{
+    // executed after model binding, action execution, result execution
+    public void OnResourceExecuted(ResourceExecutedContext context) // contains info like result returned IActionResult infos
+    {
+        Console.WriteLine("Executed!");
+    }
+
+    //executed at the start of the pipline, after author filter
+    public void OnResourceExecuting(ResourceExecutingContext context) // context contains HttpContext, routing details, info about current action
+    {
+        Console.WriteLine("Executing!!!");
+    }
+}
