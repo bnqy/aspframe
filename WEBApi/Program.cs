3004321529dc0d5dcc4be047857fa4a8718c642e
bnqy <obbonobo@gmail.com>
2024-03-28 21:55:38 +0300

    add filters to controllers and rp

--- WEBApi/Program.cs
+++ WEBApi/Program.cs
@@ -1,15 +1,30 @@
 using Microsoft.AspNetCore.Mvc.Formatters;
+using WEBApi.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<FruitService>();
 
+builder.Services.AddRazorPages() // it doesnot let you pass lambda opt so use AddMvcOpt()
+	.AddMvcOptions(opt =>
+	{
+		opt.Filters.Add(new LogResourceFilter());  // adds filters to rp
+		//opt.Filters.Add<LogResourceFilter>();
+		//opt.Filters.Add(typeof(LogResourceFilter));
+
+	});
 
 builder.Services.AddControllers(opt =>
 {
 	//opt.RespectBrowserAcceptHeader = true;
 	//opt.OutputFormatters.RemoveType<StringOutputFormatter>();
+
+	opt.Filters.Add<LogResourceFilter>();  // adds filter glabally
+										   //opt.Filters.Add(typeof(LogResourceFilter)); // same as above
+										   //opt.Filters.Add(new LogResourceFilter()); // same
+
+
 }) // adds necassery services for controllers
 	.AddXmlSerializerFormatters()  // adds XML formatter
 	.AddNewtonsoftJson()  // adds JSON formatter (newton rather then text.json)
